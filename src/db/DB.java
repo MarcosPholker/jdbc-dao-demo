ef6544957eecc948a13d3018ac7c89fdf114a0d5
ThePhobos007 <antonioleitedasilvamarcos@gmail.com>
2021-05-11 19:28:37 -0300

    firstcomit

---
+++ src/db/DB.java
@@ -0,0 +1,69 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+
+public class DB {
+	
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url,props);
+				
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties prop = new Properties();
+			prop.load(fs);
+			return prop;
+		} 
+		catch(IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	public static void CloseConnection(){
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				new DbException(e.getMessage());
+			}
+		}
+	}
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (Exception e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (Exception e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+}
